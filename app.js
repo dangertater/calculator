903c0da2120a69c835e75cc99b7d29ad58377d9f
dangertater <106702944+dangertater@users.noreply.github.com>
2022-09-26 16:27:46 -0600

    we have clear working

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-document.getElementsByClassName("inputBox")
+let input = document.getElementById("input")
+let clearButton = document.getElementById("clearButton")
+console.log(input)
 
-let episodes = document.querySelectorAll(".vevent")
+let green = false
+
+setInterval(() => {
+	if (!green) {
+		green = true
+		input.style.border = "1px solid green"
+	} else {
+		green = false
+		input.style.border = "1px solid red"
+	}
+}, 1000)
+
+//premade method below, some dude decdied that the string 'change' is going to determine when the callback, '(e)' in this case, should be called
+input.addEventListener("change", (e) => {
+	console.log("change", e)
+})
+
+input.addEventListener("keydown", (e) => {
+	console.log("keyDownEvent", e.key, input.value)
+})
+
+clearButton.addEventListener("click", (e) => {
+	input.value = ""
+	input.focus()
+	console.log("clear")
+})

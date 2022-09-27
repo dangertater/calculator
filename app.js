98680a3fb6ff44066665d4d6d027fdd8b811d4aa
dangertater <106702944+dangertater@users.noreply.github.com>
2022-09-27 16:10:58 -0600

    js deleted, pseudocode added as rough guide, html and css complete

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
-const input = document.getElementById("input")
-const clearButton = document.getElementById("clearButton")
-const buttonEquals = document.getElementById("buttonEquals")
-let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-let actions = ["Add", "Subtract", "Dot", "Divide", "Multiply"]
-let buttons = [...numbers, ...actions]
-buttons.forEach((b) => {
-	let id = "button" + b
-	let button = document.getElementById(id)
-	button.addEventListener("click", (e) => {
-		if (actions.includes(b)) {
-			let lastValue = input.value[input.value.length - 1]
-			if (numbers.includes(lastValue)) {
-				input.value = input.value + button.innerText
-			}
-		} else {
-			input.value = input.value + button.innerText
-		}
-	})
-})
+//will need const's connecting html to the JS (document.getelementID ('variable name'))
 
-clearButton.addEventListener("click", (e) => {
-	input.value = ""
-	input.focus()
-	console.log("clear")
-})
-buttonEquals.addEventListener("click", (e) => {
-	input.value = eval(input.value)
-	console.log("button = clicked")
-})
+//will need arrays for actions, numbers, and both combined into another array 'buttons'
 
-//steps since finish point 1
-//add validation for buttons
-//
-console.log("numbers", numbers)
-console.log("actions", actions)
-console.log("buttons", buttons)
+//will need a function that will cycle through (.forEach) each element in array 'buttons',
+//----then will do an '.addEventListener' comparing if actions array includes each cycled element, then comparing if numbers
+//----array includes the cycled element. and then pushing the element if in numbers (prevents actions occuring in succession)
+//----bonus, figure out how to have it delete the last element if the last element is an 'action' (will make code non functional)
 
-//asdfasdfadsf
+//both clear and equals will need their own event code for when user clicks

e7900eb86fb7f79dc7c428bed19645de47b3d375
dangertater <106702944+dangertater@users.noreply.github.com>
2022-09-28 15:01:59 -0600

    all things are a go, gonna get learnt on some Q tests right meow

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ buttons.forEach((bName) => {
 //both clear and equals will need their own event code for when user clicks
 //q4e - here we are not shouting into the void because the input.value will update the user's screen which is the goal
 //----no return necessary. is this correct thinking? eric says yes
-clears.addEventListener("click", (e) => {
+clear.addEventListener("click", (e) => {
 	input.value = ""
 	input.focus()
 })
@@ -41,3 +41,46 @@ equals.addEventListener("click", (e) => {
 //event listener/addlistener question
 //----fancy would be not getlementbyID, isntead use document.querySelectorAll (returning array of all things matching selector),
 //----then use same non inline function thend etermine which button clicked based on teh information in the event
+
+/*************************************************
+ *  Part 2, evaluate our own calculation string
+ *************************************************/
+// Create a function, that accepts a string, which is a valid thing to calculate
+// EG '11+15'
+
+// for each character, add it to the left side, as long as we haven't found an action
+// if we have found an action, add it to the right side
+// Loop through each character of the string
+// Create a left side, and a right side, and an action
+// We need to do our own calculations here
+let nums1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+let actions1 = ["+", "-", "*", "/", "^"]
+
+let calculate = (string) => {
+	let leftSide = ""
+	let rightSide = ""
+	let action = ""
+	for (let i = 0; i < string.length; i++) {
+		if (actions1.includes(string[i])) {
+			action = action + string[i]
+		} else if (action.length === 0) {
+			leftSide = leftSide + string[i]
+		} else {
+			rightSide = rightSide + string[i]
+		}
+	}
+	return {
+		leftSide,
+		rightSide,
+		action,
+	}
+}
+
+console.log(calculate("11+15"))
+console.log(calculate("12-159"))
+console.log(calculate("123+662"))
+console.log(calculate("11/15"))
+
+//if (action.length > 0) {
+// rightSide = rightSide + string[i]
+// }

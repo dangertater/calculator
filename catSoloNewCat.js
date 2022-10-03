eb8beaf53acaf0a3318b5b9e3e619258f14fa707
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-03 17:16:32 -0600

    pre eric doing the things

--- catSoloNewCat.js
+++ catSoloNewCat.js
@@ -2,20 +2,48 @@ let numsSolo = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 let powersSolo = ["^"]
 let multiplyDivideSolo = ["*", "/"]
 let addSubtractSolo = ["+", "-"]
-let allOperatorsSolo = [...powersSolo, multiplyDivideSolo, addSubtractSolo]
+let allOperatorsSolo = [
+	...powersSolo,
+	...multiplyDivideSolo,
+	...addSubtractSolo,
+]
 
-// seperates string into the different arrays
+let hasOperatorSolo = (leString) => {
+	for (let i = 0; i < leString.length; i++) {
+		if (allOperatorsSolo.includes(leString[i]) === true) return true
+	}
+}
+
+// let numberCruncher = (stringSolo) => {}
+// seperates string into the left right and operator strings, puts them in object.
+//determines if left side or right side has operators, then
 let leftRightSeperatorSolo = (stringSolo) => {
 	let leftSideSolo = ""
 	let rightSideSolo = ""
 	let operatorsSolo = ""
+	//incase there is no operator
+	if (!hasOperatorSolo(stringSolo)) {
+		return stringSolo
+	}
+	//incase string is empty
+	if (stringSolo === "") {
+		throw new Error("you fucked it up no string")
+	}
+
 	for (let i = 0; i < stringSolo.length; i++) {
 		for (let i = 0; i < stringSolo.length; i++) {
 			if (stringSolo[i] === "^") {
 				operatorsSolo = stringSolo.slice(i, i + 1)
 				leftSideSolo = stringSolo.slice(0, i)
 				rightSideSolo = stringSolo.slice(i + 1)
-				return { leftSideSolo, rightSideSolo, operatorsSolo }
+				leftSideSolo = leftRightSeperatorSolo(leftSideSolo)
+				rightSideSolo = leftRightSeperatorSolo(rightSideSolo)
+				let obj = {
+					leftSideSolo,
+					rightSideSolo,
+					operatorsSolo,
+				}
+				return obj
 			}
 		}
 		for (let i = 0; i < stringSolo.length; i++) {
@@ -23,7 +51,10 @@ let leftRightSeperatorSolo = (stringSolo) => {
 				operatorsSolo = stringSolo.slice(i, i + 1)
 				leftSideSolo = stringSolo.slice(0, i)
 				rightSideSolo = stringSolo.slice(i + 1)
-				return { leftSideSolo, rightSideSolo, operatorsSolo }
+				leftSideSolo = leftRightSeperatorSolo(leftSideSolo)
+				rightSideSolo = leftRightSeperatorSolo(rightSideSolo)
+				let obj = { leftSideSolo, rightSideSolo, operatorsSolo }
+				return obj
 			}
 		}
 		for (let i = 0; i < stringSolo.length; i++) {
@@ -31,11 +62,15 @@ let leftRightSeperatorSolo = (stringSolo) => {
 				operatorsSolo = stringSolo.slice(i, i + 1)
 				leftSideSolo = stringSolo.slice(0, i)
 				rightSideSolo = stringSolo.slice(i + 1)
-				return { leftSideSolo, rightSideSolo, operatorsSolo }
+				leftSideSolo = leftRightSeperatorSolo(leftSideSolo)
+				rightSideSolo = leftRightSeperatorSolo(rightSideSolo)
+				let obj = { leftSideSolo, rightSideSolo, operatorsSolo }
+				return obj
 			}
 		}
 	}
 }
+
 // }
 // leftRightSeperatorSolo('2+4+6+8')
 // leftRightSeperatorSolo('2+4/6^8')

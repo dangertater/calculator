8bbf759f811a9854171efa05d99accdf4c46dc6a
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-03 17:34:40 -0600

    pre fixing order of operations easy way, come back for challenge project if you want to die.

--- catSoloNewCat.js
+++ catSoloNewCat.js
@@ -13,14 +13,19 @@ let hasOperatorSolo = (leString) => {
 		if (allOperatorsSolo.includes(leString[i]) === true) return true
 	}
 }
-
+let sliceAndRedo = (stringSolo, i) => {
+	let operatorsSolo = stringSolo.slice(i, i + 1)
+	let leftSideSolo = stringSolo.slice(0, i)
+	let rightSideSolo = stringSolo.slice(i + 1)
+	leftSideSolo = leftRightSeperatorSolo(leftSideSolo)
+	rightSideSolo = leftRightSeperatorSolo(rightSideSolo)
+	let obj = { leftSideSolo, rightSideSolo, operatorsSolo }
+	return obj
+}
 // let numberCruncher = (stringSolo) => {}
 // seperates string into the left right and operator strings, puts them in object.
 //determines if left side or right side has operators, then
 let leftRightSeperatorSolo = (stringSolo) => {
-	let leftSideSolo = ""
-	let rightSideSolo = ""
-	let operatorsSolo = ""
 	//incase there is no operator
 	if (!hasOperatorSolo(stringSolo)) {
 		return stringSolo
@@ -31,45 +36,34 @@ let leftRightSeperatorSolo = (stringSolo) => {
 	}
 
 	for (let i = 0; i < stringSolo.length; i++) {
-		for (let i = 0; i < stringSolo.length; i++) {
-			if (stringSolo[i] === "^") {
-				operatorsSolo = stringSolo.slice(i, i + 1)
-				leftSideSolo = stringSolo.slice(0, i)
-				rightSideSolo = stringSolo.slice(i + 1)
-				leftSideSolo = leftRightSeperatorSolo(leftSideSolo)
-				rightSideSolo = leftRightSeperatorSolo(rightSideSolo)
-				let obj = {
-					leftSideSolo,
-					rightSideSolo,
-					operatorsSolo,
-				}
-				return obj
-			}
+		if (stringSolo[i] === "^") {
+			return sliceAndRedo(stringSolo, i)
+			// let operatorsSolo = stringSolo.slice(i, i + 1)
+			// let leftSideSolo = stringSolo.slice(0, i)
+			// let rightSideSolo = stringSolo.slice(i + 1)
+			// //below few lines are functionally the same, left for gettin learnt.
+			// let rightSideSoloParsed = leftRightSeperatorSolo(rightSideSolo)
+			// let obj = {
+			// 	leftSideSolo: leftRightSeperatorSolo(leftSideSolo),
+			// 	rightSideSolo: rightSideSoloParsed,
+			// 	operatorsSolo,
+			// }
+			// return obj
 		}
-		for (let i = 0; i < stringSolo.length; i++) {
-			if (stringSolo[i] === "*" || stringSolo[i] === "/") {
-				operatorsSolo = stringSolo.slice(i, i + 1)
-				leftSideSolo = stringSolo.slice(0, i)
-				rightSideSolo = stringSolo.slice(i + 1)
-				leftSideSolo = leftRightSeperatorSolo(leftSideSolo)
-				rightSideSolo = leftRightSeperatorSolo(rightSideSolo)
-				let obj = { leftSideSolo, rightSideSolo, operatorsSolo }
-				return obj
-			}
+	}
+	for (let i = 0; i < stringSolo.length; i++) {
+		if (stringSolo[i] === "*" || stringSolo[i] === "/") {
+			return sliceAndRedo(stringSolo, i)
 		}
-		for (let i = 0; i < stringSolo.length; i++) {
-			if (stringSolo[i] === "+" || stringSolo[i] === "-") {
-				operatorsSolo = stringSolo.slice(i, i + 1)
-				leftSideSolo = stringSolo.slice(0, i)
-				rightSideSolo = stringSolo.slice(i + 1)
-				leftSideSolo = leftRightSeperatorSolo(leftSideSolo)
-				rightSideSolo = leftRightSeperatorSolo(rightSideSolo)
-				let obj = { leftSideSolo, rightSideSolo, operatorsSolo }
-				return obj
-			}
+	}
+	for (let i = 0; i < stringSolo.length; i++) {
+		if (stringSolo[i] === "+" || stringSolo[i] === "-") {
+			return sliceAndRedo(stringSolo, i)
 		}
 	}
 }
+
+//goal, take the objects reverse engineer to a single number (solution), dont forget order of operations
 
 // }
 // leftRightSeperatorSolo('2+4+6+8')
